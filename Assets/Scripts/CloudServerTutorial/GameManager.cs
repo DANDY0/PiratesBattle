f6964dfb182b821558d1c2def39dbdad4c2ca840
Danylo_Nesvitailo <nesvitailo_danylo@granddevs.com>
2024-04-30 11:54:02 +0300

    4-GameManager & Levels

--- Assets/Scripts/CloudServerTutorial/GameManager.cs
+++ Assets/Scripts/CloudServerTutorial/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using Photon.Pun;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
 
 namespace CloudServerTutorial
 {
@@ -26,5 +28,50 @@ namespace CloudServerTutorial
         }
 
         #endregion
+        
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+                return;
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        #endregion
+        
+        #region Photon Callbacks
+
+        public override void OnPlayerEnteredRoom(Photon.Realtime.Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Photon.Realtime.Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        #endregion
+        
+        
     }
 }
